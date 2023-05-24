008b30f33c9f1cf6ebd93e5d3576e0f2b0315f10
cyber-yuito723 <129262838+cyber-yuito723@users.noreply.github.com>
2023-05-24 11:56:08 +0900

    2023-5-24

--- TeaChimer-publicver-1.00/run.py
+++ TeaChimer-publicver-1.00/run.py
@@ -2,3 +2,12 @@
 # ©️ 2023 Contributors to the TeaChimer project #
 ################################################
 
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.messagebox as mbox
+import pygame
+
+class SoundBoard:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("TeaChimer-publicver-1.00")
