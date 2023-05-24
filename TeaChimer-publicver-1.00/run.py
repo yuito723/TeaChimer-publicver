53f94ca7efa8a2cf80a41acfd29a1f42b93bf6a8
cyber-yuito723 <129262838+cyber-yuito723@users.noreply.github.com>
2023-05-24 17:04:09 +0900

    2023-5-24

--- TeaChimer-publicver-1.00/run.py
+++ TeaChimer-publicver-1.00/run.py
@@ -1,6 +1,28 @@
 ################################################
 # ©️ 2023 Contributors to the TeaChimer project #
 ################################################
+# TeaChimer is free software:                  #
+# you can redistribute it and/or modify it     #
+# under the terms of                           #
+# the GNU General Public License as published  #
+# by the Free Software Foundation,             #
+# either version 3 of the License,             #
+# or (at your option) any later version.       #
+#                                              #
+# This program is distributed in the hope      #
+# that it will be useful,                      #
+# but WITHOUT ANY WARRANTY;                    #
+# without even the implied warranty            #
+# of MERCHANTABILITY or FITNESS                #
+# FOR A PARTICULAR PURPOSE.                    #
+# See the GNU General Public License           #
+# for more details.                            #
+#                                              #
+# You should have received a copy of           #
+# the GNU General Public License               #
+# along with this program. If not, see         #
+# <https://www.gnu.org/licenses/>.             #
+################################################
 
 import tkinter as tk
 import tkinter.ttk as ttk
