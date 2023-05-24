e8cd67ce757cee95e1cf683a3f5f95cd9177e47f
cyber-yuito723 <129262838+cyber-yuito723@users.noreply.github.com>
2023-05-24 13:18:22 +0900

    Update run.py

--- TeaChimer-publicver-1.00/run.py
+++ TeaChimer-publicver-1.00/run.py
@@ -12,3 +12,7 @@ class SoundBoard:
         self.master = master
         self.master.title("TeaChimer-pulicver-1.00")
         self.master.attributes("-fullscreen", True)
+        self.master.configure(background="white")
+        
+        self.notebook = ttk.Notebook(self.master)
+        self.notebook.pack(fill=tk.BOTH, expand=True)
